73d79bd66983ad51cac4948ecf5e7f116fca8f37
steven-yn <ssungyeon94@gmail.com>
2024-01-05 05:10:32 +0900

    initial commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <RedTitle />
+    <CountButton />
+  </div>
+</template>
---
+++ components/CountButton.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};
+</script>
+
+<template>
+  <!-- make this button work -->
+  <button @click="increment">count is: {{ count }}</button>
+  <button v-on:click="increment">count is: {{ count }}</button>
+</template>

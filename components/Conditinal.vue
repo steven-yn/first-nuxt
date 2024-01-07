340b4f5dd185937d7c6cb64faa14baaadaf0c14c
윤성연 <sungyeon.yoon@yunseong-yeon-ui-MacBookAir.local>
2024-01-07 23:28:54 +0900

    add: 2 tutorials

---
+++ components/Conditinal.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+
+const awesome = ref(true);
+
+function toggle() {
+  awesome.value = !awesome.value;
+}
+</script>
+
+<template>
+  <button @click="toggle">토글 버튼</button>
+  <h1 v-if="awesome">Vue는 굉장해! 엄청나!</h1>
+  <h1 v-else-if="!awesome">오 안돼 😢</h1>
+</template>
---
+++ components/ControledInput.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+
+const text = ref("");
+function onInput(e) {
+  // v-on 핸들러는 네이티브 DOM 이벤트를 인자로 받습니다.
+  text.value = e.target.value;
+}
+
+const vModelText = ref("");
+</script>
+
+<template>
+  <input :value="text" @input="onInput" placeholder="여기에 입력하기" />
+  <p>{{ text }}</p>
+  <input v-model="vModelText" placeholder="여기에 입력하기" />
+  <p>{{ vModelText }}</p>
+</template>
